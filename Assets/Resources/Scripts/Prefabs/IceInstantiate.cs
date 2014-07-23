60bb6743e85ec8d104ef76746533dcdfbc8e5789
Brandan Haertel <foilpudding@gmail.com>
2014-07-23 12:43:33 -0700

    Ice Prefab

---
+++ Assets/Resources/Scripts/Prefabs/IceInstantiate.cs
@@ -0,0 +1,74 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class IceInstantiate : MonoBehaviour {
+
+	GameObject mIceParticle;
+	int life, count, magnitude, x, y, xinc, yinc;
+
+	// Use this for initialization
+	void Start () {
+		mIceParticle = Resources.Load("Prefabs/IceParticle") as GameObject;
+		life = 100;
+		magnitude = 0;
+		count = 0;
+		x = 0;
+		y = 0;
+	}
+	
+	
+	// Update is called once per frame
+	void Update () {
+		if(life-- <= 0)
+			Destroy(this.gameObject);
+		//squareEffect();
+		roundEffect();
+	}
+	
+	void roundEffect(){
+		for(;;){
+			if(count-- == 0 ){
+				magnitude += 3;
+				count = magnitude * 4;
+				return;
+			}
+			spawn();
+			Vector3 r = Random.insideUnitCircle;
+			r.Normalize();
+			r *= magnitude;
+			x = (int) r.x;
+			y = (int) r.y;
+		}
+	}
+	
+	void squareEffect(){
+		for(;;){
+			if(count-- == 0 ){
+				magnitude += 3;
+				count = magnitude * 4;
+				x = -magnitude;
+				y = 0;
+				xinc = 3;
+				yinc = 3;
+				return;
+			}
+			spawn();
+			if(Mathf.Abs(x += xinc) == magnitude){
+				xinc *= -1;
+			}
+			if(Mathf.Abs(y += yinc) == magnitude){
+				yinc *= -1;
+			}
+		}
+	}
+	
+	void spawn(){
+		GameObject e = Instantiate(mIceParticle) as GameObject;
+		IceParticle spawnedParticle = e.GetComponent<IceParticle>();
+		if(spawnedParticle != null) {
+			e.transform.position = this.transform.position + new Vector3(x,y,0);
+			//spawnedParticle.transform.Rotate(new Vector3(0,0,r));
+			//e.transform.position += e.transform.up;
+		}
+	}
+}
---
+++ Assets/Resources/Scripts/Prefabs/IceParticle.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class IceParticle : MonoBehaviour {
+
+	public Sprite ice01, ice02, ice03, ice04, ice05;
+	int life;
+	
+	// Use this for initialization
+	void Start () {
+		randomSprite();
+		life = Random.Range(1,50);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(life-- <= 0)
+			Destroy(this.gameObject);
+	}
+	
+	private void randomSprite(){
+		SpriteRenderer mSpriteRenderer = GetComponent<SpriteRenderer>();
+		switch(Random.Range(0,5)) {
+			case (0):
+				mSpriteRenderer.sprite = ice01;
+				break;
+			case 1:
+				mSpriteRenderer.sprite = ice02;
+				break;
+			case 2:
+				mSpriteRenderer.sprite = ice03;
+				break;
+			case 3:
+				mSpriteRenderer.sprite = ice04;
+				break;
+			case 4:
+				mSpriteRenderer.sprite = ice05;
+				break;
+		}
+	}
+}
