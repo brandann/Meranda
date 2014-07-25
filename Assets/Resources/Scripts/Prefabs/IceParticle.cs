521ac0d76c7c91261ae094af26812d96dd36225b
Brandan Haertel <foilpudding@gmail.com>
2014-07-25 12:31:10 -0700

    fine tune ice

--- Assets/Resources/Scripts/Prefabs/IceParticle.cs
+++ Assets/Resources/Scripts/Prefabs/IceParticle.cs
@@ -7,17 +7,31 @@ public class IceParticle : MonoBehaviour {
 	int life;
 	public int lifeMin;
 	public int lifeMax;
+	float scale = .1f;
+	bool rand = true;
 	
 	// Use this for initialization
 	void Start () {
 		randomSprite();
 		life = Random.Range(lifeMin, lifeMax);
+		if(rand){
+			this.transform.localScale = new Vector3(.1f,.1f,1f);
+			this.transform.Rotate(new Vector3(0,0,Random.Range(0,360)));
+		}
 	}
 	
 	// Update is called once per frame
 	void Update () {
 		if(life-- <= 0)
 			Destroy(this.gameObject);
+		if(rand){
+			this.transform.localScale += new Vector3(scale,scale,0f);
+			randomDirection();
+		}
+	}
+	
+	private void randomDirection(){
+		this.transform.position += this.transform.up * .2f;
 	}
 	
 	private void randomSprite(){
